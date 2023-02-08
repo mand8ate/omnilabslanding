153a005de1177be532198e4f7b600894b2cb745c
mand8ate <mand8ate@gmail.com>
2023-02-08 22:35:50 +0900

    work in progress translatinmg page to japanese

--- js/language.js
+++ js/language.js
@@ -3,7 +3,7 @@ const languageButtonMobile = document.getElementById("language-button-mobile");
 
 const changeLanguage = (button) => {
   button.addEventListener("click", () => {
-    if (button.innerHTML === "En/Ja") {
+    if (button.innerHTML === "日本語") {
       window.location.href = "index-ja.html";
     } else {
       window.location.href = "index.html";
